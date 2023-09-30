26e877d26c2890af30469a7ab5c9e9557311a31c
Igor Augusto <igoraugustobz@gmail.com>
2023-09-30 10:52:01 -0300

    feat: funcionalidade do placar dinâmico adicionada

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,28 @@ buttonHiddenText.addEventListener("click", function () {
     hiddenText.classList.toggle("hidden");
 });
 
-// Lógica para atualizar as datas nos elementos HTML com a classe 'date-card'
+// Lógica para deixar o placar do último jogo dinâmico
+function placarDinamico() {
+
+    const scoreTeams = document.querySelectorAll('p.size-scoreboard');
+
+    // Geração de números aleatórios entre 1 e 5
+    const placar = [
+        Math.floor(Math.random() * (6 - 1)) + 1,
+        Math.floor(Math.random() * (6 - 1)) + 1
+    ];
+
+    // Iteração sobre os elementos que possuem a classe size-scoreboard
+    for (let i = 0; i < scoreTeams.length; i++) {
+
+        scoreTeams[i].innerHTML = `${placar[i]}`;
+    }
+
+}
+
+placarDinamico();
+
+// Lógica para deixar as datas dinâmicas
 function dataAtualDecrescente() {
 
     // Objeto Date e elementos <p>
@@ -20,9 +41,9 @@ function dataAtualDecrescente() {
 
     // Geração de números aleatórios entre 1 e 30
     const numbers = [
-        Math.floor(Math.random() * (28 - 1)) + 1,
-        Math.floor(Math.random() * (28 - 1)) + 1,
-        Math.floor(Math.random() * (28 - 1)) + 1
+        Math.floor(Math.random() * (29 - 1)) + 1,
+        Math.floor(Math.random() * (29 - 1)) + 1,
+        Math.floor(Math.random() * (29 - 1)) + 1
     ];
 
     // Ordear os números em ordem decrescente
