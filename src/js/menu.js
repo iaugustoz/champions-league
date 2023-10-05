3386fcbfc43b895e76642adbf82245f35ecbdec8
Igor Augusto <igoraugustobz@gmail.com>
2023-10-05 11:51:37 -0300

    feat: funcionalidade de fechamento do menu por campo de visão adicionada

--- src/js/menu.js
+++ src/js/menu.js
@@ -6,14 +6,20 @@ const listMenu = document.getElementById('list-menu');
 // Variável para controlar o estado do menu
 let menuVisible = false;
 
-menu.addEventListener('click', function () {
+// Função para verificar se o elemento está no campo de visão
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
 
-    // Definindo as animações para o menu deslizante
-    const slideIn = [
-        { transform: 'translateX(100%)', easing: 'ease-out' },
-        { transform: 'translateX(0%)', easing: 'ease-out' }
-    ];
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 
+// Função para fechar a lista suspensa com a animação de SlideOut
+function closeMenu() {
     const slideOut = [
         { transform: 'translateX(0%)', easing: 'ease-out' },
         { transform: 'translateX(100%)', easing: 'ease-out' }
@@ -23,16 +29,31 @@ menu.addEventListener('click', function () {
         duration: 800,
     };
 
+    const slideAnimation = slide.animate(slideOut, timing);
+    slideAnimation.onfinish = function () {
+        listMenu.classList.add('hidden'); // Oculta a lista de itens do menu após a animação
+    };
+
+    menuVisible = false; // Define o menu como invisível
+}
+
+// Evento de clique no menu
+menu.addEventListener('click', function () {
+    // Definindo as animações para o menu deslizante
+    const slideIn = [
+        { transform: 'translateX(100%)', easing: 'ease-out' },
+        { transform: 'translateX(0%)', easing: 'ease-out' }
+    ];
+
+    const timing = {
+        duration: 800,
+    };
+
     // Verifica se o menu está visível ou não
     if (menuVisible) {
-
         // Se estiver visível, anima para torná-lo invisível
-        const slideAnimation = slide.animate(slideOut, timing);
-        slideAnimation.onfinish = function () {
-            listMenu.classList.add('hidden'); // Oculta a lista de itens do menu após a animação
-        };
+        closeMenu();
     } else {
-
         // Caso contrário, anima para torná-lo visível
         listMenu.classList.remove('hidden'); // Exibe a lista de itens do menu
         const slideAnimation = slide.animate(slideIn, timing);
@@ -42,29 +63,25 @@ menu.addEventListener('click', function () {
     menuVisible = !menuVisible;
 });
 
-// Obtendo referências aos itens do menu
+// Obtém referências aos itens do menu
 const itensMenu = document.getElementsByClassName("itens-menu");
 
 // Adiciona eventos de clique aos itens do menu
 for (let i = 0; i < itensMenu.length; i++) {
     itensMenu[i].addEventListener("click", function () {
-
-        // Define a animação de saída do menu deslizante
-        const slideOut = [
-            { transform: 'translateX(0%)', easing: 'ease-out' },
-            { transform: 'translateX(100%)', easing: 'ease-out' }
-        ];
-
-        const timing = {
-            duration: 800,
-        };
-
-        // Inicia a animação de saída
-        const slideAnimation = slide.animate(slideOut, timing);
-        slideAnimation.onfinish = function () {
-            listMenu.classList.add('hidden'); // Oculta a lista de itens do menu após a animação
-        };
-
-        menuVisible = false; // Garante que o menu esteja definido como invisível após clicar em um item do menu
+        closeMenu(); // Fecha o menu quando um item do menu é clicado
     });
 }
+
+// Verifica a visibilidade do elemento e fecha o menu se estiver fora do campo de visão
+function checkVisibility() {
+    if (!isElementInViewport(menu) && menuVisible) {
+        closeMenu();
+    }
+}
+
+// Adiciona um evento de rolagem (scroll) para verificar a visibilidade continuamente
+window.addEventListener('scroll', checkVisibility);
+
+// Verifica a visibilidade inicialmente
+checkVisibility();
